f2971c290402feba17260b312c2c9c2d5cf37e81
GalileuGalilei <alfredocossetin@outlook.com>
2023-06-17 23:00:04 -0300

    hyperlink e video

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 #pesquise um site no google e o abra usando selenium
 #pesquise um site no google e o abra usando selenium
+__package__ = 'selenium'
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,6 +10,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import unittest
+import validators
+
+#Button
+#Search 
+#Top Bar
+#scroll
+#hide things
+#imagens
+#video
+#hyperlink 
+#text field
 
 class DenverUniversityTestCase(unittest.TestCase):
 
@@ -38,6 +50,60 @@ class DenverUniversityTestCase(unittest.TestCase):
         self.assertIn('https://www.du.edu/academics/undergraduate-programs?search=', self.browser.current_url)
         time.sleep(2)
 
+    def test_video_play(self):
+        iframe = self.browser.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div/div/div/div/div/div/div/iframe')
+        actions = ActionChains(self.browser)
+        actions.scroll_to_element(iframe).perform()
+        time.sleep(1)
+        #play no vídeo
+        iframe.click()
+        #espera o vídeo chegar a 6 segundos
+        time.sleep(6)
+        #verifica se o vídeo está rodando
+        self.assertTrue(iframe.is_displayed())
+        #pausa o vídeo
+        iframe.click()
+        time.sleep(1)
+
+    def test_video_title(self):
+        iframe = self.browser.find_element(By.XPATH, '//*[@id="main-content"]/div[2]/div/div/div/div/div/div/div/iframe')
+        actions = ActionChains(self.browser)
+        actions.scroll_to_element(iframe).perform()
+        time.sleep(1)
+        self.browser.switch_to.frame(iframe)
+        #pega o título do vídeo
+        title = self.browser.find_element(By.CLASS_NAME, 'ytp-title-link').text
+        self.assertIn('University of Denver', title)
+        
+    def test_hyperlink_using_url_validator(self):
+        link = self.browser.find_element(By.XPATH, '//*[@id="main-content"]/div[1]/div[2]/p[2]/a[1]')
+        actions = ActionChains(self.browser)
+        actions.scroll_to_element(link).perform()
+        time.sleep(1)
+        #valida se o link é valido
+        self.assertTrue(validators.url(str(link.get_attribute('href'))))
+        time.sleep(1)
+        #clica no link
+        link.click()
+
+    def test_hyperlink_using_window_title(self):
+        link = self.browser.find_element(By.XPATH, '//*[@id="main-content"]/div[1]/div[2]/p[2]/a[1]')
+        actions = ActionChains(self.browser)
+        actions.scroll_to_element(link).perform()
+        time.sleep(1)
+        #pega o link
+        link_url = link.get_attribute('href')
+        #clica no link
+        link.click()
+        time.sleep(2)
+        #troca para a nova aba aberta
+        self.browser.switch_to.window(self.browser.window_handles[1])
+        time.sleep(1)
+        #verifica se o link levou ao lugar certo
+        self.assertIn('Kennedy Mountain Campus', self.browser.title)
+
+    
+        
         # pesquisa AMOGUS
     #     string = 'AMOGUS'
     #     search = self.browser.find_element(By.ID, 'search-input')
@@ -73,5 +139,5 @@ class DenverUniversityTestCase(unittest.TestCase):
     #     coisa.click()
     #     time.sleep(2)
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     unittest.main(verbosity=2)
